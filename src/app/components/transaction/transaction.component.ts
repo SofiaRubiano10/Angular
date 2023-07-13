917602b7b878c59f790cc165f97d4c34095cea93
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-07-13 12:59:26 +0200

    Prueba de está sirviendo el Click

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -10,10 +10,17 @@ export class TransactionComponent {
   @Input() transaction!: Transaction;
 
   dateStyles: Record<string, string> = {
-    'border-bottom':
-      this.transaction?.type === 'expense'
-        ? '1px solid red'
-        : '1px solid green',
     'margin-top': '3px',
   };
+
+  //funcion ejecutada al momento de montar el componente
+  ngOnInit() {
+    this.dateStyles['border-bottom'] =
+      this.transaction?.type === 'expense'
+        ? '1px solid red'
+        : '1px solid green';
+  }
+  miFuncion() {
+    console.log('Me hicieron click');
+  }
 }
